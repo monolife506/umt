e260ca7809ec4bc7af7114c8a4a6c7027a85596b
SIJIN <skd6150@gmail.com>
2019-11-12 21:18:43 +0900

    mysql 설치 및 설정

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 let express = require('express');
+let path = require('path');
 let app = express();
 
-app.get('/', function (req, res) {
-    res.send('umt!');
+app.get('/', function(req, res) {
+    res.send('hello world');
 });
 
 app.listen(3000, function () {
