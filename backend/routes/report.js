2a55f7ea58b233f2694556f3d63967eb8d615a92
SIJIN <skd6150@gmail.com>
2019-11-14 02:44:14 +0900

    implement report api

--- backend/routes/report.js
+++ backend/routes/report.js
@@ -6,20 +6,29 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
     let connection = await mysql.createConnection(config.mysql);
     let phoneNumber = req.query.phoneNumber;
-
-    // let affiliation = req.query.affiliation;
-    // let food = req.query.food;
-    // let date = req.query.date;
+    let affiliation = req.query.affiliation;
+    let food = req.query.food;
+    let occuredTime = req.query.occuredTime;
 
     let deliveryManInfo = await connection.query(
-        'SELECT * FROM `deliveryManList` WHERE phoneNumber = phoneNumber',
+        'SELECT * FROM `deliveryManList` WHERE phoneNumber = ?',
         [phoneNumber]
     );
-
-    // let insert_deliveryList = await req.mysql.query(
-    //     'INSERT INTO `deliverylist`(`deliveryManId`, `food`, `shopId`, `occuredTime`, `isCrime`) VALUES(?, ?, ?, ?, ?, ?)',
-    //     [deliveryManId, food, ]
-    // );
+    if(deliveryManInfo.length === 0) {
+        console.log("new");
+        await connection.query(
+            'INSERT INTO deliveryManList(phoneNumber, deliveryManName, orderCount, crimeCount, affiliation) VALUES (?, "익명", 0 , 0, ?)',
+            [phoneNumber, affiliation]
+        );
+        deliveryManInfo = await connection.query(
+            'SELECT * FROM `deliveryManList` WHERE phoneNumber = ?',
+            [phoneNumber]
+        );
+    }
+    let deliveryInfo = await connection.query(
+        'INSERT INTO deliveryList(deliveryManId, shopId, food, occuredTime, isCrime) VALUES (?, ?, ?, ?, ?)',
+        [deliveryManInfo[0].deliveryManId, 2, food, occuredTime, 1]
+    );
 });
 
 module.exports = router;
